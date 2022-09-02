ba3b8991046d277d65e4eaf8d5031015bbf72957
rijorijom <112694009+rijorijom@users.noreply.github.com>
2022-09-02 14:58:16 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,7 +2,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 class Translation(object):
 
-    START_TEXT = """Hello {},
+    START_TEXT = """👋 Hi {},
 
 I Am Telegram URL Uploader Bot.
 
